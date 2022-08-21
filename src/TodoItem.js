d50db3ad233f64b6443b3ea4f49f467b01bc6b53
Josué Alarcón <josuealarco@gmail.com>
2022-08-21 15:28:18 -0500

    Optimizing conditions

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,13 +5,9 @@ const TodoItem = ({text, completed}) => {
     <>
         <li className="bg-white rounded-3xl w-10/12 flex p-3 items-center">
             <button className="h-7 w-7 bg-gradient-to-b from-indigo-500 to-blue-500 rounded-full text-center">
-                {completed  === false ?
-                <span className="block rounded-full h-5 w-5 bg-white mx-auto"> </span> :
-                <span className="h-5 w-5 bg-green-300 rounded-full mx-auto flex justify-center items-center">{<MdCheck />}</span> } 
+                <span className={`flex rounded-full h-5 w-5  mx-auto ${completed ? "bg-green-300 justify-center items-center":"bg-white"}`}>{completed && <MdCheck />}</span> 
             </button>
-              
                 <p  className={`ml-4 ${completed && "line-through text-neutral-700"}`}>{text}</p>
-        
         </li>
     </> );
 }
